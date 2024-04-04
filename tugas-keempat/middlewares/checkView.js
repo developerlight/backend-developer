1fa810c13ed8165fea805dc447ec646459db7e0b
Samsul Muhyidin <128915331+developerlight@users.noreply.github.com>
2024-04-04 16:21:14 +0700

    fix cache

--- tugas-keempat/middlewares/checkView.js
+++ tugas-keempat/middlewares/checkView.js
@@ -1,36 +1,45 @@
-const cache = require('../utils/cache.js');
-const { WithParams } = require('../utils/query.js')
+const cache = require("../utils/cache.js");
+const { WithParams } = require("../utils/query.js");
 
 const checkView = async (req, res, next) => {
-    try {
-        const { nim } = req.params;
-        if (!nim) {
-            return res.status(400).send('NIM is required')
-        }
-        
-        const result = await WithParams(
-            'SELECT * FROM mahasiswa WHERE nim = ?',
-            [nim]
-        )
-
-        if (result.length === 0) {
-            return res.status(404).send('Data not found')
-        }
-
-        const view = result[0].view + 1
-        const value = cache.get('/v2/get')
-        const valueJSON = (value ? JSON.parse(value) : value)
-        cache.set('/v2/get', value ? JSON.stringify(valueJSON?.map((data) => ({...data, view: data.nim == nim ? view : data.view}))) : undefined)
-
-        await WithParams(
-            'UPDATE mahasiswa SET view = ? WHERE nim = ?',
-            [view, nim]
-        )
-
-        next()
-    } catch (error) {
-        res.status(500).json({ message: error.message });
+  try {
+    const { nim } = req.params;
+    if (!nim) {
+      return res.status(400).send("NIM is required");
     }
-}
+
+    const result = await WithParams("SELECT * FROM mahasiswa WHERE nim = ?", [
+      nim,
+    ]);
+
+    if (result.length === 0) {
+      return res.status(404).send("Data not found");
+    }
+
+    const view = result[0].view + 1;
+    const value = cache.get("/v2/get");
+    const valueJSON = value ? JSON.parse(value) : value;
+    cache.set(
+      "/v2/get",
+      value
+        ? JSON.stringify(
+            valueJSON?.map((data) => ({
+              ...data,
+              view: data.nim == nim ? view : data.view,
+            }))
+          )
+        : undefined
+    );
+
+    await WithParams("UPDATE mahasiswa SET view = ? WHERE nim = ?", [
+      view,
+      nim,
+    ]);
+
+    next();
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
 
 module.exports = checkView;
